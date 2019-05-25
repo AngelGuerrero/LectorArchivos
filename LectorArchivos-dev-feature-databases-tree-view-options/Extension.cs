e7bda1086cdef0f0751e7dae08b8c7b33b698de9
Ángel Guerrero <lasantiagoguerrero@gmail.com>
2019-05-24 21:01:51 -0500

    Prueba para carga de archivos múltiples servidores

---
+++ LectorArchivos-dev-feature-databases-tree-view-options/Extension.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Security.Principal;
+using System.Windows.Forms;
+
+namespace LecturaDeArchivos
+{
+    public static class Extension
+    {
+        public static void AppendText(this RichTextBox box, string text, Color color)
+        {
+            box.SelectionStart = box.TextLength;
+            box.SelectionLength = 0;
+
+            box.SelectionColor = color;
+            box.AppendText(text);
+            box.AppendText(Environment.NewLine);
+            box.AppendText(Environment.NewLine);
+            box.SelectionColor = box.ForeColor;
+        }
+
+        public static IEnumerable<T> AllControls<T>(this Control startingPoint) where T : Control
+        {
+            bool hit = startingPoint is T;
+            if (hit)
+            {
+                yield return startingPoint as T;
+            }
+            foreach (var child in startingPoint.Controls.Cast<Control>())
+            {
+                foreach (var item in AllControls<T>(child))
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Obtiene todos los controles de un formulario con recursividad
+        /// </summary>
+        /// <param name="control"></param>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static IEnumerable<Control> GetAllControlsOfType(this Control control, Type type)
+        {
+            var controls = control.Controls.Cast<Control>();
+
+            return controls.SelectMany(Control => GetAllControlsOfType(Control, type))
+                           .Concat(controls)
+                           .Where(c => c.GetType() == type)
+                           ;
+        }
+
+
+
+        public static IEnumerable<Control> GetAllControls(this Control container)
+        {
+            IEnumerable<Control> controls = container.Controls.Cast<Control>();
+
+            foreach (Control control in controls)
+            {
+                yield return control;
+                GetAllControls(control);
+            }
+        }
+
+
+
+        public static IEnumerable<string> ObtenerMetodos(Type tipo)
+        {
+            foreach (var metodo in tipo.GetMethods()) yield return metodo.Name;
+        }
+
+
+
+        public static bool esAdministrador()
+        {
+            using (WindowsIdentity identity = WindowsIdentity.GetCurrent())
+            {
+                return new WindowsPrincipal(identity).IsInRole(WindowsBuiltInRole.Administrator);
+            }
+        }
+
+    }
+}
---
+++ LectorArchivos-dev-feature-databases-tree-view-options/FormFilterOptions.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LecturaDeArchivos
+{
+    public partial class FormFilterOptions : Form
+    {
+        public string Options { get; set; } = "";
+
+        public string Filter { get; set; } = "";
+
+        public FormMain formMain;
+
+        public FormFilterOptions()
+        {
+            InitializeComponent();
+        }
+
+        private void FormFilterOptions_Load(object sender, EventArgs e)
+        {
+            //
+            // Inicializa el botón
+            radioBtnEquals.Checked = true;
+
+            //
+            // Get the instance of FormMain
+            formMain = Application.OpenForms.OfType<Form>().First() as FormMain;
+        }
+
+        private List<string> ApplyOptions(string pDatabase)
+        {
+            //
+            // Return values
+            List<string> retList = new List<string>();
+
+            //
+            // Get the stored procedures
+            var storedProcedures = Utils.GetStoredProceduresFromCurrentDatabase(formMain.RemoteConnection, pDatabase);
+
+            //
+            // It gets the tables using the current connection.
+            var tables = Utils.GetTablesFromCurrentDatabase(formMain.RemoteConnection, pDatabase);
+
+            if (Options.Equals("Igual"))
+            {
+                //
+                // Filter stored procedures
+                storedProcedures = storedProcedures.Where(sp => sp.ToLower().Equals(Filter.ToLower()))
+                                                   .ToList();
+
+                //
+                // Filter the tables
+                tables = tables.Where(table => table.ToLower().Equals(Filter.ToLower()))
+                               .ToList();
+
+            }
+            else if (Options.Equals("Contiene"))
+            {
+                //
+                // Filter the stored procedures
+                storedProcedures = storedProcedures.Where(sp => sp.ToLower().Contains(Filter.ToLower()))
+                                                   .ToList();
+
+                //
+                // Filter the tables
+                tables = tables.Where(table => table.ToLower().Contains(Filter.ToLower()))
+                               .ToList();
+
+            }
+
+            //
+            // Merge the list
+            List<string> li_1 = new List<string>();
+            List<string> li_2 = new List<string>();
+
+            storedProcedures.ForEach(sp => li_1.Add($"Stored Procedure: {sp}"));
+            tables.ForEach(t => li_2.Add($"Table: {t}"));
+
+            return li_1.Union(li_2).ToList();
+        }
+
+        private void radioBtnEquals_CheckedChanged(object sender, EventArgs e)
+        {
+            foreach (Control control in this.groupBox.Controls)
+            {
+                if (control is RadioButton)
+                {
+                    RadioButton radio = control as RadioButton;
+                    if (radio.Checked)
+                    {
+                        Options = radio.Text;
+                    }
+                }
+            }
+        }
+
+        private void txtFilter_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtFilter.Text)) return;
+
+            Filter = txtFilter.Text;
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            //
+            // Validations
+            if (formMain.SelectedDatabasesList.Count <= 0)
+            {
+                MessageBox.Show("No se ha seleccionado ninguna base de datos", "Filtrado", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!formMain.RemoteConnection.Connected)
+            {
+                MessageBox.Show("No se ha establecido la conexión con el servidor", "Error de conexión", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            //
+            // Clear the log
+            richTextBoxLog.Text = string.Empty;
+
+            formMain.SelectedDatabasesList.ForEach(selecteddb =>
+            {
+                string msg = "";
+
+                //
+                // Test the connection for each databases selected
+                lblStatus.Text = $"Estableciendo conexión con {selecteddb}";
+                if (!formMain.RemoteConnection.TestConnectionnWindowsAuth(formMain.CurrentServer.Name, ref msg, selecteddb))
+                {
+                    lblStatus.Text = msg;
+                    MessageBox.Show(msg, "Error de conexión", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                //
+                // Write the output in the RichTextBox
+                richTextBoxLog.AppendText($"Servidor: {formMain.CurrentServer.Name}, Base de datos: {selecteddb} {Environment.NewLine}");
+
+                var listResult = ApplyOptions(selecteddb);
+
+                if (listResult.Count <= 0)
+                {
+                    richTextBoxLog.AppendText($"\t- Not found: {Filter}", Color.Red);
+                    return;
+                }
+
+                listResult.ForEach(result => richTextBoxLog.AppendText($"\t- Found: {result}", Color.Green));
+            });
+        }
+
+    }
+}
